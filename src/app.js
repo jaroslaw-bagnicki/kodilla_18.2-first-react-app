9436f391aed923627cfbc09974327c622887e30f
Jarosław Bagnicki <jarek@bagnicki.net>
2019-02-10 06:14:20 +0100

    Add poster to Movie component

--- src/app.js
+++ src/app.js
@@ -2,19 +2,22 @@ var movies = [
   {
     id: 1,
     title: 'Harry Potter',
-    desc: 'Film o czarodzieju'
+    desc: 'Film o czarodzieju',
+    poster: 'https://img.buzzfeed.com/buzzfeed-static/static/2018-08/22/13/asset/buzzfeed-prod-web-02/sub-buzz-8759-1534959485-1.jpg'
   },
   {
     id: 2,
     title: 'Król Lew',
-    desc: 'Film o królu sawanny'
+    desc: 'Film o królu sawanny',
+    poster: 'https://eu.movieposter.com/posters/archive/main/108/MPW-54063'
   }
 ];
 
 var moviesElements = movies.map(function(movie) {
   return React.createElement('li', {key: movie.id},
     React.createElement('h2', {}, movie.title),
-    React.createElement('p', {}, movie.desc)
+    React.createElement('p', {}, movie.desc),
+    React.createElement('img', {src: movie.poster})
   );
 });
 
